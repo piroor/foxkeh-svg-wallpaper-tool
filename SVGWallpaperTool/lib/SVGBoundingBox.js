190be65c09914c952cef0804e2d49432f20949d3
ynakajima <yuhta.nakajima@gmail.com>
2011-04-11 21:39:55 +0900

    SVGBoundingBox?SVGSprite??????svgSprite?????

--- SVGWallpaperTool/lib/SVGBoundingBox.js
+++ SVGWallpaperTool/lib/SVGBoundingBox.js
@@ -1,11 +1,11 @@
 (function(){
     
-    var SVGBoundingBox = function(SVGSprite,options) {
+    var SVGBoundingBox = function(svgSprite,options) {
          
         options = (typeof options == "undefined")? {} : options;
         
-        //SVGSprite
-        this.SVGSprite = SVGSprite;
+        //svgSprite
+        this.svgSprite = svgSprite;
         
 	this.isSafari = (navigator.userAgent.indexOf("Safari") != -1 && navigator.userAgent.indexOf("Chrome") == -1);
 	
@@ -18,8 +18,8 @@
         
         this.center = document.createElementNS("http://www.w3.org/2000/svg", "circle");
         this.point = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        this.SVGSprite.svgElement.ownerSVGElement.appendChild(this.center);
-        this.SVGSprite.svgElement.ownerSVGElement.appendChild(this.point);
+        this.svgSprite.svgElement.ownerSVGElement.appendChild(this.center);
+        this.svgSprite.svgElement.ownerSVGElement.appendChild(this.point);
         
         
         //初期化
@@ -33,8 +33,8 @@
     SVGBoundingBox.prototype.init = function(){
 
         this.origMouseX = this.origMouseY = this._rotateInitRotate = null;      
-        this.origWidth = this.SVGSprite.width;
-        this.origHeight = this.SVGSprite.height;
+        this.origWidth = this.svgSprite.width;
+        this.origHeight = this.svgSprite.height;
         
         //枠を作る
         var _bbox = document.createElementNS("http://www.w3.org/2000/svg", "rect");
@@ -46,7 +46,7 @@
         var _scaleBox = document.createElementNS("http://www.w3.org/2000/svg", "g");
 	/*var _scaleBoxSVG = document.createElementNS("http://www.w3.org/2000/svg", "use");
         _scaleBoxSVG.setAttributeNS("http://www.w3.org/1999/xlink","xlink:href","svg/scale.svg#scale");
-        _scaleBoxSVG.setAttribute("transform", "scale("+1/this.SVGSprite._viewPortScaleX+")");
+        _scaleBoxSVG.setAttribute("transform", "scale("+1/this.svgSprite._viewPortScaleX+")");
 	_scaleBox.appendChild(_scaleBoxSVG);
 	*/
 	var _scaleSVGSrc = '<svg xmlns="http://www.w3.org/2000/svg">';
@@ -66,14 +66,14 @@
 	var parser = new DOMParser()
 	var _scaleSVGXML = parser.parseFromString(_scaleSVGSrc, "text/xml");
 	var _scaleSVG = document.adoptNode(_scaleSVGXML.getElementById("scale"));
-	_scaleSVG.setAttribute("transform", "scale("+1/this.SVGSprite._viewPortScaleX+")");
+	_scaleSVG.setAttribute("transform", "scale("+1/this.svgSprite._viewPortScaleX+")");
 	_scaleBox.appendChild(_scaleSVG);
         this._scaleBox = _scaleBox;
         
         //回転ボックス
         var _rotateBox = document.createElementNS("http://www.w3.org/2000/svg", "g");
         //var _rotateBoxSVG = document.createElementNS("http://www.w3.org/2000/svg", "use");
-        //_rotateBoxSVG.setAttribute("transform", "scale("+1/this.SVGSprite._viewPortScaleX+")");
+        //_rotateBoxSVG.setAttribute("transform", "scale("+1/this.svgSprite._viewPortScaleX+")");
         //_rotateBoxSVG.setAttributeNS("http://www.w3.org/1999/xlink","xlink:href","svg/rotate.svg#rotate");
         //_rotateBox.appendChild(_rotateBoxSVG);
 	var _rotateSVGSrc = '<svg xmlns="http://www.w3.org/2000/svg">';
@@ -92,7 +92,7 @@
 	_rotateSVGSrc += '</svg>';
 	var _rotateSVGXML = (new DOMParser()).parseFromString(_rotateSVGSrc, "text/xml");
 	var _rotateSVG = document.adoptNode(_rotateSVGXML.getElementById("rotate"));
-	_rotateSVG.setAttribute("transform", "scale("+1/this.SVGSprite._viewPortScaleX+")");
+	_rotateSVG.setAttribute("transform", "scale("+1/this.svgSprite._viewPortScaleX+")");
 	_rotateBox.appendChild(_rotateSVG);
 	this._rotateBox = _rotateBox;
         
@@ -102,7 +102,7 @@
         $(this._scaleBox).mousedown( function(e){ self._startScale(); });
         $(this._rotateBox).mousedown( function(e){ self._startRotate(); });
         	
-	//this.SVGSprite.svgElement.ownerSVGElement.addEventListener("mousemove", function(e){
+	//this.svgSprite.svgElement.ownerSVGElement.addEventListener("mousemove", function(e){
 	$(window).mousemove(function(e){
 	    
             self._doScale(e);
@@ -125,38 +125,38 @@
      */
     SVGBoundingBox.prototype._setBoxes = function(){
         
-        var strokeWidth = (20/Math.abs(this.SVGSprite.scaleX))/this.SVGSprite._viewPortScaleX;
-        var boxWidth = (20/this.SVGSprite.scaleX);
+        var strokeWidth = (20/Math.abs(this.svgSprite.scaleX))/this.svgSprite._viewPortScaleX;
+        var boxWidth = (20/this.svgSprite.scaleX);
         
         //枠
         var _bbox = this._bbox;
-        _bbox.setAttribute("x", this.SVGSprite._svgTransformUtil.origOffset.x);
-        _bbox.setAttribute("y", this.SVGSprite._svgTransformUtil.origOffset.y);
+        _bbox.setAttribute("x", this.svgSprite._svgTransformUtil.origOffset.x);
+        _bbox.setAttribute("y", this.svgSprite._svgTransformUtil.origOffset.y);
         _bbox.setAttribute("style", "opacity:0.6; fill:#fff; stroke:#fff; stroke-width:"+strokeWidth+"; stroke-linejoin:round;");
         
         //拡大縮小
         var _scaleBox = this._scaleBox;
-        //_scaleBox.setAttribute("x", (this.SVGSprite._svgTransformUtil.origOffset.x+this.origWidth-(boxWidth/3))*this.SVGSprite.scaleX);
-        //_scaleBox.setAttribute("y", (this.SVGSprite._svgTransformUtil.origOffset.y+this.origHeight-(boxWidth/3))*this.SVGSprite.scaleX);
-        var _scaleX = (this.SVGSprite._svgTransformUtil.origOffset.x+this.origWidth-(boxWidth*.5))*this.SVGSprite.scaleX;
-        var _scaleY = (this.SVGSprite._svgTransformUtil.origOffset.y+this.origHeight-(boxWidth*.5))*this.SVGSprite.scaleX;
-        _scaleBox.setAttribute("transform", "scale("+1/this.SVGSprite.scaleX+") translate("+_scaleX+", "+_scaleY+")");
+        //_scaleBox.setAttribute("x", (this.svgSprite._svgTransformUtil.origOffset.x+this.origWidth-(boxWidth/3))*this.svgSprite.scaleX);
+        //_scaleBox.setAttribute("y", (this.svgSprite._svgTransformUtil.origOffset.y+this.origHeight-(boxWidth/3))*this.svgSprite.scaleX);
+        var _scaleX = (this.svgSprite._svgTransformUtil.origOffset.x+this.origWidth-(boxWidth*.5))*this.svgSprite.scaleX;
+        var _scaleY = (this.svgSprite._svgTransformUtil.origOffset.y+this.origHeight-(boxWidth*.5))*this.svgSprite.scaleX;
+        _scaleBox.setAttribute("transform", "scale("+1/this.svgSprite.scaleX+") translate("+_scaleX+", "+_scaleY+")");
         //_scaleBox.setAttribute("width", boxWidth);
         //_scaleBox.setAttribute("height", boxWidth);
-        //_scaleBox.setAttribute("style", "opacity:1; fill:#fff; stroke:#000; stroke-width:"+(.5/this.SVGSprite.scaleX)+"; stroke-linejoin:round;cursor:se-resize");
+        //_scaleBox.setAttribute("style", "opacity:1; fill:#fff; stroke:#000; stroke-width:"+(.5/this.svgSprite.scaleX)+"; stroke-linejoin:round;cursor:se-resize");
         _scaleBox.setAttribute("style", "cursor:se-resize");
         
         
         //回転
         var _rotateBox = this._rotateBox;
-        //_rotateBox.setAttribute("x", ((this.SVGSprite._svgTransformUtil.origOffset.x-(boxWidth*.6))*(this.SVGSprite.scaleX)));
-        //_rotateBox.setAttribute("y", ((this.SVGSprite._svgTransformUtil.origOffset.y-(boxWidth*.6))*(this.SVGSprite.scaleX)));
-        var _rotateX = (this.SVGSprite._svgTransformUtil.origOffset.x-(boxWidth*.8))*(this.SVGSprite.scaleX);
-        var _rotateY = (this.SVGSprite._svgTransformUtil.origOffset.y-(boxWidth*.8))*(this.SVGSprite.scaleX);
-        _rotateBox.setAttribute("transform", "scale("+1/this.SVGSprite.scaleX+") translate("+_rotateX+", "+_rotateY+")");
+        //_rotateBox.setAttribute("x", ((this.svgSprite._svgTransformUtil.origOffset.x-(boxWidth*.6))*(this.svgSprite.scaleX)));
+        //_rotateBox.setAttribute("y", ((this.svgSprite._svgTransformUtil.origOffset.y-(boxWidth*.6))*(this.svgSprite.scaleX)));
+        var _rotateX = (this.svgSprite._svgTransformUtil.origOffset.x-(boxWidth*.8))*(this.svgSprite.scaleX);
+        var _rotateY = (this.svgSprite._svgTransformUtil.origOffset.y-(boxWidth*.8))*(this.svgSprite.scaleX);
+        _rotateBox.setAttribute("transform", "scale("+1/this.svgSprite.scaleX+") translate("+_rotateX+", "+_rotateY+")");
         //_rotateBox.setAttribute("width", boxWidth);
         //_rotateBox.setAttribute("height", boxWidth);
-        //_rotateBox.setAttribute("style", "opacity:1; fill:#eee; stroke:#000; stroke-width:"+(.5/this.SVGSprite.scaleX)+"; stroke-linejoin:round;cursor:pointer");
+        //_rotateBox.setAttribute("style", "opacity:1; fill:#eee; stroke:#000; stroke-width:"+(.5/this.svgSprite.scaleX)+"; stroke-linejoin:round;cursor:pointer");
         _rotateBox.setAttribute("style", "cursor:pointer");
 
     };
@@ -167,14 +167,14 @@
     SVGBoundingBox.prototype.enable = function(){
         
         //枠を挿入
-        this.SVGSprite._svgTransformUtil.transformWrapper.appendChild(this._bbox);
-        this.SVGSprite._svgTransformUtil.transformWrapper.appendChild(this.SVGSprite._oapcityElement);
+        this.svgSprite._svgTransformUtil.transformWrapper.appendChild(this._bbox);
+        this.svgSprite._svgTransformUtil.transformWrapper.appendChild(this.svgSprite._oapcityElement);
         
         //拡大縮小ボックスを挿入
-        this.SVGSprite._svgTransformUtil.transformWrapper.appendChild(this._scaleBox);
+        this.svgSprite._svgTransformUtil.transformWrapper.appendChild(this._scaleBox);
         
         //回転ボックスを挿入
-        this.SVGSprite._svgTransformUtil.transformWrapper.appendChild(this._rotateBox);
+        this.svgSprite._svgTransformUtil.transformWrapper.appendChild(this._rotateBox);
         
     };
     
@@ -184,13 +184,13 @@
     SVGBoundingBox.prototype.disable = function(){
 
         //枠を削除
-        this.SVGSprite._svgTransformUtil.transformWrapper.removeChild(this._bbox);
+        this.svgSprite._svgTransformUtil.transformWrapper.removeChild(this._bbox);
         
         //拡大縮小ボックスを削除
-        this.SVGSprite._svgTransformUtil.transformWrapper.removeChild(this._scaleBox);
+        this.svgSprite._svgTransformUtil.transformWrapper.removeChild(this._scaleBox);
         
         //回転ボックスを削除
-        this.SVGSprite._svgTransformUtil.transformWrapper.removeChild(this._rotateBox);
+        this.svgSprite._svgTransformUtil.transformWrapper.removeChild(this._rotateBox);
         
     };
     
@@ -200,8 +200,8 @@
     SVGBoundingBox.prototype._startScale = function(){
 
         this._scaling = true;
-        this._scaleOrigWidth = this.SVGSprite.width;
-        this._scaleOrigHeight = this.SVGSprite.height;
+        this._scaleOrigWidth = this.svgSprite.width;
+        this._scaleOrigHeight = this.svgSprite.height;
         
     };
     
@@ -222,19 +222,19 @@
 
             /*            
             //移動量
-            var x = ((event.clientX-this.origMouseX)/this.SVGSprite._viewPortScaleX);
-            var y = ((event.clientY-this.origMouseY)/this.SVGSprite._viewPortScaleY);
+            var x = ((event.clientX-this.origMouseX)/this.svgSprite._viewPortScaleX);
+            var y = ((event.clientY-this.origMouseY)/this.svgSprite._viewPortScaleY);
             var _scale = Math.sqrt(x*x+y*y); //√(c-a)^2+(d-b)^2
             _scale = (x<0||y<0)? -_scale : _scale;
             
-            this.SVGSprite.width = this._scaleOrigWidth+_scale;
-	    this.SVGSprite.scaleY = this.SVGSprite.scaleX;
-            this.SVGSprite.height = this._scaleOrigHeight+_scale;
+            this.svgSprite.width = this._scaleOrigWidth+_scale;
+	    this.svgSprite.scaleY = this.svgSprite.scaleX;
+            this.svgSprite.height = this._scaleOrigHeight+_scale;
 	    */
 	    
 	    if(this.origMouseX == null) {
 		
-		this.origScreenCTM = this.SVGSprite._svgTransformUtil.transformWrapper.getScreenCTM();
+		this.origScreenCTM = this.svgSprite._svgTransformUtil.transformWrapper.getScreenCTM();
 	    
 	    }
 	    
@@ -243,7 +243,7 @@
 	    
 	    var ctm = this.origScreenCTM;
 			
-	    var p = this.SVGSprite.svgElement.ownerSVGElement.createSVGPoint();
+	    var p = this.svgSprite.svgElement.ownerSVGElement.createSVGPoint();
 	    p.x = x;
 	    p.y = y;
 	    p = p.matrixTransform(ctm.inverse());
@@ -251,15 +251,15 @@
             if(this.origMouseX == null) {
             
                 this.origMouseX = p.x;
-		this.origScaleX = this.SVGSprite.scaleX;
-		this.origFlipped = ((this.SVGSprite.scaleY<0 && this.origScaleX>0) || (this.SVGSprite.scaleY>0 && this.origScaleX<0));
+		this.origScaleX = this.svgSprite.scaleX;
+		this.origFlipped = ((this.svgSprite.scaleY<0 && this.origScaleX>0) || (this.svgSprite.scaleY>0 && this.origScaleX<0));
 		
             }
 	    
-	    //var cp = this.SVGSprite.svgElement.ownerSVGElement.createSVGPoint();
-	    //cp.x = this.SVGSprite.width/2;//this.SVGSprite.x;
-	    //cp.y = this.SVGSprite.height/2;//this.SVGSprite.y;
-	    //cp = cp.matrixTransform(this.SVGSprite._svgTransformUtil.transformWrapper.getCTM().inverse());
+	    //var cp = this.svgSprite.svgElement.ownerSVGElement.createSVGPoint();
+	    //cp.x = this.svgSprite.width/2;//this.svgSprite.x;
+	    //cp.y = this.svgSprite.height/2;//this.svgSprite.y;
+	    //cp = cp.matrixTransform(this.svgSprite._svgTransformUtil.transformWrapper.getCTM().inverse());
 	    
 	    /*this.point.setAttribute("cx", p.x);
             this.point.setAttribute("cy", p.y);
@@ -270,8 +270,8 @@
 	    var ratio = (p.x-(this.origMouseX/2))/(this.origMouseX/2); //中心点を基準とした初期座標とマウス座標の差分
 	    var scale = this.origScaleX*ratio;
 	    	    
-	    this.SVGSprite.scaleX = scale;
-	    this.SVGSprite.scaleY = (this.origFlipped)? -scale : scale;
+	    this.svgSprite.scaleX = scale;
+	    this.svgSprite.scaleY = (this.origFlipped)? -scale : scale;
 	    
             this._setBoxes();
                         
@@ -296,9 +296,9 @@
     SVGBoundingBox.prototype._startRotate = function(){
 
         this._rotation = true;
-        this._rotateOrigRotation = this.SVGSprite.rotation;
-        this._rotateOrigX = this.SVGSprite.x;
-        this._rotateOrigY = this.SVGSprite.y;
+        this._rotateOrigRotation = this.svgSprite.rotation;
+        this._rotateOrigX = this.svgSprite.x;
+        this._rotateOrigY = this.svgSprite.y;
         
     };
     
@@ -309,13 +309,13 @@
                 
         if(this._rotation) {
             
-            var clientX = event.layerX/this.SVGSprite._viewPortScaleX;
-            var clientY = event.layerY/this.SVGSprite._viewPortScaleX;
+            var clientX = event.layerX/this.svgSprite._viewPortScaleX;
+            var clientY = event.layerY/this.svgSprite._viewPortScaleX;
             
             if(this._rotateInitRotate == null) {
                 
-                var _x = (clientX*this.SVGSprite._viewPortScaleX)-this._rotateOrigX;
-                var _y = (clientY*this.SVGSprite._viewPortScaleY)-this._rotateOrigY;
+                var _x = (clientX*this.svgSprite._viewPortScaleX)-this._rotateOrigX;
+                var _y = (clientY*this.svgSprite._viewPortScaleY)-this._rotateOrigY;
                 var _radian = Math.atan2(_y,_x);
                 var _rotation = _radian/(Math.PI/180);
                 this._rotateInitRotate = _rotation;
@@ -343,7 +343,7 @@
             this.point.setAttribute("fill", "#f00");
 	    */
             
-            this.SVGSprite.rotation = this._rotateOrigRotation+rotation;
+            this.svgSprite.rotation = this._rotateOrigRotation+rotation;
             
             this._setBoxes();
                         
